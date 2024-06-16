e6008db7b56302093735ac3ccfcaa2ca60e035bd
otonielnn <otoniel_2002@live.com>
2024-06-15 23:29:25 -0300

    StreamAPI - Consumer<T>
    Signed-off-by: otonielnn <otoniel_2002@live.com>

---
+++ POO/SteamAPI/src/functional_interface/examples/ConsumerExample.java
@@ -0,0 +1,26 @@
+package functional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static <T> void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        Consumer<Integer> imprimirNumeroPar = (numero) -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach(imprimirNumeroPar);
+
+        //
+        numeros.stream().forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        });
+    }
+}
